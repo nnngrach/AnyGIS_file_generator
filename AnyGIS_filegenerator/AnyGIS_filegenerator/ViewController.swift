deff2590199ccad3797d06417a564097d89b83ae
nnngrach <nnngrach@gmail.com>
2019-03-25 22:33:04 +0300

    Recreating project. Install SQL framework

---
+++ AnyGIS_filegenerator/AnyGIS_filegenerator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AnyGIS_filegenerator
+//
+//  Created by HR_book on 25/03/2019.
+//  Copyright © 2019 H.Rach. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
