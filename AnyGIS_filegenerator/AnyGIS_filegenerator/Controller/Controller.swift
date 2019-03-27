184f20db141d7c7804f9c5821fe016a9433e1dad
nnngrach <nnngrach@gmail.com>
2019-03-27 21:14:35 +0300

    Add main controller

---
+++ AnyGIS_filegenerator/AnyGIS_filegenerator/Controller/Controller.swift
@@ -0,0 +1,41 @@
+//
+//  Controller.swift
+//  AnyGIS_filegenerator
+//
+//  Created by HR_book on 27/03/2019.
+//  Copyright © 2019 H.Rach. All rights reserved.
+//
+
+import Foundation
+
+class Controller {
+    
+    public func generateAll() {
+    
+    }
+    
+    public func generateInstallersForLocus() {
+        
+    }
+    
+    public func generateMapsForLocus() {
+        
+    }
+    
+    public func generateMapsForGuru() {
+        
+    }
+    
+    public func generateMapsForOrux() {
+        
+    }
+    
+    public func generateMapsForOsmand() {
+        
+    }
+    
+    public func generateWebPages() {
+        
+    }
+    
+}
