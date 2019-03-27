67ba776661a30a88415c67d13e6b27ddd8a39112
nnngrach <nnngrach@gmail.com>
2019-03-27 17:39:59 +0300

    Make working sqlitedb genertor (sketch)

---
+++ AnyGIS_filegenerator/AnyGIS_filegenerator/Model of Data/Text templates/UrlTemplates.swift
@@ -0,0 +1,66 @@
+//
+//  UrlTemplates.swift
+//  AnyGIS_filegenerator
+//
+//  Created by HR_book on 27/03/2019.
+//  Copyright © 2019 H.Rach. All rights reserved.
+//
+
+import Foundation
+
+struct UrlTemplates {
+    
+    let dataBasePatch = "file://///Projects/GIS/AnyGIS_file_generator/AnyGIS_filegenerator/test.sqlitedb"
+    
+    
+    let localPathToMarkdownPages = "file:////Projects/GIS/Online%20map%20sources/map-sources/Web/Html/Download/"
+    
+    let localPathToIcons = "file:////Projects/GIS/Online%20map%20sources/map-sources/Locus_online_maps/Icons/"
+    let localPathToInstallers = "file:////Projects/GIS/Online%20map%20sources/map-sources/Locus_online_maps/Installers/"
+    let localPathToLocusMapsFull = "file:////Projects/GIS/Online%20map%20sources/map-sources/Locus_online_maps/Maps_full/"
+    let localPathToLocusMapsShort = "file:////Projects/GIS/Online%20map%20sources/map-sources/Locus_online_maps/Maps_short/"
+    let localPathToGuruMapsFull = "file:////Projects/GIS/Online%20map%20sources/map-sources/Galileo_online_maps/Maps_full/"
+    let localPathToGuruMapsShort = "file:////Projects/GIS/Online%20map%20sources/map-sources/Galileo_online_maps/Maps_short/"
+    let localPathToOsmandMapsFull = "file:////Projects/GIS/Online%20map%20sources/map-sources/Osmand_online_maps/Maps_full/"
+    let localPathToOsmandMapsShort = "file:////Projects/GIS/Online%20map%20sources/map-sources/Osmand_online_maps/Maps_short/"
+    
+    
+    let localPathToGuruMapsInServer = "file://///Projects/GIS/AnyGIS%20server/AnyGIS_Server/Public/galileo/"
+    let localPathToOruxMapsFullInServer = "file:////Projects/GIS/AnyGIS%20server/AnyGIS_Server/Public/orux_full/"
+    let localPathToOruxMapsShortInServer = "file:////Projects/GIS/AnyGIS%20server/AnyGIS_Server/Public/orux_short/"
+    
+    let localPathToOsmandTemplate = "file://///Projects/GIS/AnyGIS%20server/AnyGIS_Server/osmandTemplate.sqlitedb"
+    
+    let gitLocusInstallersFolder = "https://github.com/nnngrach/AnyGIS_maps/master/Locus_online_maps/Installers/"
+    let gitLocusIconsFolder = "https://github.com/nnngrach/AnyGIS_maps/raw/master/Locus_online_maps/Icons/"
+    let gitLocusPagesFolder = "https://raw.githubusercontent.com/nnngrach/AnyGIS_maps/master/Web/Html/Download/"
+    
+    let gitLocusMapsFolder = "https://raw.githubusercontent.com/nnngrach/AnyGIS_maps/master/Locus_online_maps/Maps_full/"
+    let anygisGuruMapsFolder = "https://anygis.herokuapp.com/download/galileo/"
+    
+    
+    let gitLocusFullMapsZip = "https://minhaskamal.github.io/DownGit/#/home?url=https://github.com/nnngrach/AnyGIS_maps/tree/master/Locus_online_maps/Maps_full"
+    let gitLocusShortMapsZip = "https://minhaskamal.github.io/DownGit/#/home?url=https://github.com/nnngrach/AnyGIS_maps/tree/master/Locus_online_maps/Maps_short"
+    
+    let gitLocusActionInstallersFolder = "locus-actions://https/raw.githubusercontent.com/nnngrach/AnyGIS_maps/master/Locus_online_maps/Installers/"
+    let gitGuruActionInstallersFolder = "guru://open?path=https://raw.githubusercontent.com/nnngrach/AnyGIS_maps/master/Galileo_online_maps/Maps_full/"
+    
+    
+    
+    
+    let indexPage = "https://nnngrach.github.io/AnyGIS_maps/index"
+    let descriptionPage = "https://nnngrach.github.io/AnyGIS_maps/Web/Html/Description"
+    let rusOutdoorPage = "https://nnngrach.github.io/AnyGIS_maps/Web/Html/RusOutdoor"
+    let downloadPage = "https://nnngrach.github.io/AnyGIS_maps/Web/Html/DownloadPage"
+    let locusPage = "https://nnngrach.github.io/AnyGIS_maps/Web/Html/Locus"
+    let guruPage = "https://nnngrach.github.io/AnyGIS_maps/Web/Html/Galileo"
+    let oruxPage = "https://nnngrach.github.io/AnyGIS_maps/Web/Html/Orux"
+    let osmandPage = "https://nnngrach.github.io/AnyGIS_maps/Web/Html/Osmand"
+    let apiPage = "https://nnngrach.github.io/AnyGIS_maps/Web/Html/Api"
+    
+    let anygisMapUrl = "https://anygis.herokuapp.com/{mapName}/{x}/{y}/{z}"
+    let anygisMapUrlHttp = "http://anygis.herokuapp.com/{mapName}/{x}/{y}/{z}"
+    
+    
+    let email = "anygis@bk.ru"
+}
--- AnyGIS_filegenerator/AnyGIS_filegenerator/Model of Logic/FileGenerator Tools/Osmand/SqlitedbHandler.swift
+++ AnyGIS_filegenerator/AnyGIS_filegenerator/Model of Logic/FileGenerator Tools/Osmand/SqlitedbHandler.swift
@@ -10,8 +10,89 @@ import SQLite
 
 class SqlitedbHandler {
     
+    let templates = UrlTemplates.init()
+    
+    
+    
     func test() throws {
-        let db = try Connection("file://///Projects/GIS/AnyGIS_file_generator/AnyGIS_filegenerator/test.sqlitedb")
+        
+        let patch = templates.localPathToOsmandMapsFull + "filename.sqlitedb"
+        
+        let db = try Connection(patch)
+        
+        let users = Table("info")
+        
+        let minzoom = Expression<String?>("minzoom")
+        let maxzoom = Expression<String?>("maxzoom")
+        let url = Expression<String?>("url")
+        let ellipsoid = Expression<Int64?>("ellipsoid")
+        let tilenumbering = Expression<String?>("tilenumbering")
+        let timecolumn = Expression<String?>("timecolumn")
+        let expireminutes = Expression<String?>("expireminutes")
+        let rule = Expression<String?>("rule")
+        
+        try db.run(users.create { t in
+            t.column(minzoom)
+            t.column(maxzoom)
+            t.column(url)
+            t.column(ellipsoid)
+            t.column(tilenumbering)
+            t.column(timecolumn)
+            t.column(expireminutes)
+            t.column(rule)
+        })
+        
+        try db.run(users.insert(minzoom <- "-3",
+                                maxzoom <- "16",
+                                url <- "http://anygis.herokuapp.com/Osm_Cycle_Map/{1}/{2}/{0}",
+                                ellipsoid <- 0,
+                                tilenumbering <- "BigPlanet",
+                                timecolumn <- "0",
+                                expireminutes <- "0",
+                                rule <- nil
+        ))
+        
+        
+        
+        
+        let metadata = Table("android_metadata")
+        
+        let locale = Expression<String?>("locale")
+        
+        try db.run(metadata.create { t in
+            t.column(locale)
+        })
+        
+        try db.run(metadata.insert(or: .replace,
+                                locale <- "ru_RU"
+        ))
+        
+        
+        
+        
+        let tiles = Table("tiles")
+        
+        let x = Expression<Int64>("x")
+        let y = Expression<Int64>("y")
+        let z = Expression<Int64>("z")
+        let s = Expression<Int64?>("s")
+        let image = Expression<SQLite.Blob?>("image")
+        
+        try db.run(tiles.create { t in
+            t.column(x)
+            t.column(y)
+            t.column(z)
+            t.column(s)
+            t.column(image)
+            t.primaryKey(x, y, z)
+//            t.primaryKey(s)
+        })
+        
+        try db.run(tiles.createIndex(x))
+        try db.run(tiles.createIndex(y))
+        try db.run(tiles.createIndex(z))
+        try db.run(tiles.createIndex(s))
+        
     }
     
     
